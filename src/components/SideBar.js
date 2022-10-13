0699f5fb23933b8d2430d38671001fd2170488b6
BlackHawk0 <danielngoys@outlook.com>
2022-10-13 10:57:42 +0300

    style the navbar i cons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,14 +12,14 @@ function SideBar() {
                 <img src={profile} alt='daniel' className='block w-6 h-auto my-2.5 mx-auto'/>
             </Link>
             <nav className='block text-center absolute h-56 top-1/2 w-full -mt-32'>
-                <NavLink exact="true" className={(navData) => (navData.isActive ? "active" : 'none')} to="/">
-                    <FontAwesomeIcon icon={faHome} className="hover:text-yellow-500" />
+                <NavLink exact="true" className="active" to="/">
+                    <FontAwesomeIcon icon={faHome} className="home" />
                 </NavLink>
-                <NavLink exact="true" className={(navData) => (navData.isActive ? "active" : 'none')} to="/about">
-                    <FontAwesomeIcon icon={faUser}  className="hover:text-yellow-500" />
+                <NavLink exact="true" className="active" to="/about">
+                    <FontAwesomeIcon icon={faUser}  className="about" />
                 </NavLink>
-                <NavLink exact="true" className={(navData) => (navData.isActive ? "active" : 'none')} to="/contact">
-                    <FontAwesomeIcon icon={faEnvelope}  className="hover:text-yellow-500"/>
+                <NavLink exact="true" className="active" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope}  className="contact"/>
                 </NavLink>
             </nav>
             
